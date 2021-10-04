2f503c411ac0c34c6f93e1b5812fcdbaa59957dc
Glauber Guimaraes <1615993+glauber-guimaraes@users.noreply.github.com>
2021-10-04 10:00:41 -0300

    fix: don't save player state in editor

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "Player state", menuName = "Player state", order = 0)]
 public class PlayerState : ScriptableObject
 {
+    [field: NonSerialized]
     public bool CanMove { get; set; }
+    
+
+    [field: NonSerialized]
     public bool CanJump { get; set; }
 }
